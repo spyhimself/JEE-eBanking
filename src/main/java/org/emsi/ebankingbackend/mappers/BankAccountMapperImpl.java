be48503fb9346830bce26f6e7d941d5dda877fbf
TARAR Ayman <83256011+spyhimself@users.noreply.github.com>
2023-06-19 23:55:25 +0100

    Added to the DTO layer, the Customer class (getters n setters)

---
+++ src/main/java/org/emsi/ebankingbackend/mappers/BankAccountMapperImpl.java
@@ -0,0 +1,14 @@
+package org.emsi.ebankingbackend.mappers;
+
+import org.emsi.ebankingbackend.dtos.CustomerDTO;
+import org.emsi.ebankingbackend.entities.Customer;
+
+public class BankAccountMapperImpl {
+    public CustomerDTO fromCustomer(Customer customer) {
+        return null;
+    }
+    public Customer fromCustomerDTO(CustomerDTO customerDTO) {
+        return null;
+    }
+
+}
